c0fc296f4f17b76bf47aecfb5f2211d8ecaed4a7
Daniele Greco <daniele.greco5@studio.unibo.it>
2025-11-23 22:29:23 +0100

    Completed SimpleController.java

--- 93-mvc-io/src/main/java/it/unibo/mvc/Controller.java
+++ 93-mvc-io/src/main/java/it/unibo/mvc/Controller.java
@@ -1,8 +1,32 @@
 package it.unibo.mvc;
 
+import java.util.List;
+
 /**
  *
  */
 public interface Controller {
 
+    /**
+     * 
+     * @param nexString next string to print
+     */
+    void setNextString(String nextString);
+
+    /**
+     * 
+     * @return next string to print
+    */
+    String getNextString();
+
+    /**
+     * 
+     * @return the history of the string printed
+     */
+    List<String> getHistory();
+
+    /**
+     * 
+     */
+    void printCurrentString();
 }
--- 93-mvc-io/src/main/java/it/unibo/mvc/SimpleController.java
+++ 93-mvc-io/src/main/java/it/unibo/mvc/SimpleController.java
@@ -1,9 +1,51 @@
 package it.unibo.mvc;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+
 /**
  * 
  *
  */
 public final class SimpleController implements Controller {
+
+    private List<String> history;
+    private String nextString;
+
+    public SimpleController() {
+        this.history = new LinkedList<>();
+    }
+
+    @Override
+    public List<String> getHistory() {
+        return Collections.unmodifiableList(history);
+    }
+
+    @Override
+    public String getNextString() {
+        return this.nextString;
+    }
+
+    @Override
+    public void printCurrentString() {
+        if (this.nextString == null) {
+            throw new IllegalArgumentException("string is unset");
+        } else {
+            System.out.println(this.nextString); //NOPMD
+            history.add(this.nextString);
+        }
+    }
+
+    @Override
+    public void setNextString(String nextString) {
+        if (nextString == null) {
+            throw new IllegalArgumentException("nextString is null");
+        } else {
+            this.nextString = nextString;
+        }
+        
+    }
 
 }
