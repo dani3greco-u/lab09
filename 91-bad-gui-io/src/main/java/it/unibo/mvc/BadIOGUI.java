5738a84622db9de59c25d3acb537b7d2ce5f41f9
Daniele Greco <daniele.greco5@studio.unibo.it>
2025-11-23 17:38:56 +0100

    Completed exercise 91.1.1

--- 91-bad-gui-io/src/main/java/it/unibo/mvc/BadIOGUI.java
+++ 91-bad-gui-io/src/main/java/it/unibo/mvc/BadIOGUI.java
@@ -41,9 +41,13 @@ public class BadIOGUI {
      */
     public BadIOGUI() {
         final JPanel canvas = new JPanel();
+        final JPanel newCanvas = new JPanel();
         canvas.setLayout(new BorderLayout());
+        newCanvas.setLayout(new BoxLayout(newCanvas, BoxLayout.X_AXIS));
         final JButton write = new JButton("Write on file");
-        canvas.add(write, BorderLayout.CENTER);
+        //canvas.add(write, BorderLayout.CENTER);
+        canvas.add(newCanvas, BorderLayout.CENTER);
+        newCanvas.add(write);
         frame.setContentPane(canvas);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         /*
@@ -88,6 +92,7 @@ public class BadIOGUI {
          * on screen. Results may vary, but it is generally the best choice.
          */
         frame.setLocationByPlatform(true);
+        frame.pack();
         /*
          * OK, ready to push the frame onscreen
          */
