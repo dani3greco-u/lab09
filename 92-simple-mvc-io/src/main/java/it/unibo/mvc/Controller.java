d82fc8bb007f2e7e5231095ddcadbb51618eded6
Daniele Greco <daniele.greco5@studio.unibo.it>
2025-11-23 18:26:14 +0100

    Completed Controller.java

--- 92-simple-mvc-io/src/main/java/it/unibo/mvc/Controller.java
+++ 92-simple-mvc-io/src/main/java/it/unibo/mvc/Controller.java
@@ -1,9 +1,64 @@
 package it.unibo.mvc;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.nio.charset.StandardCharsets;
 
 /**
  * Application controller. Performs the I/O.
  */
 public class Controller {
+    
+    private static final String PATH = System.getProperty("user.home");
+    private static final String DAFUALT_FILE = "output.txt";
 
+    private File currentFile = new File(PATH + File.separator + DAFUALT_FILE);
+
+    /**
+     * Set a new currentFile.
+     * 
+     * @param f
+     * @throws IllegalArgumentException
+     */
+    void setCurrentFile(File f) throws IllegalArgumentException {
+        File p = f.getParentFile();
+        if(p.exists()) {
+            this.currentFile = f;
+        }
+        else {
+            throw new IllegalArgumentException("Not found " + f.getPath());
+        }
+    }
+
+    /**
+     * Return the currentFile.
+     * 
+     * @return the currentFile
+     */
+    File getCurrenFile() {
+        return this.currentFile;
+    }
+
+    /**
+     * Return the path of currentFile.
+     * 
+     * @return the path of currentFile
+     */
+    String getCurrentFilePath() {
+        return this.currentFile.getPath();
+    }
+
+    /**
+     * Write on currentFile.
+     * 
+     * @param input
+     * @throws IOException
+     */
+    void writeOnCurrentFile(String input) throws IOException{
+        try (PrintStream ps = new PrintStream(PATH, StandardCharsets.UTF_8)) {
+            ps.print(input);
+        }
+    }
+    
 }
